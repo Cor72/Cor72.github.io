070b28561040391b1f0e30afa5bd4c0478476f24
Cor72 <Qiguo72@outlook.com>
2025-10-14 13:28:34 +0800

    Site updated: 2025-10-14 13:28:33

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1760418324590";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760419704137";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
