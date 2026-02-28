923f1817a9c2b161e149c7fc13d3bcc4d2236d1c
Cor72 <Qiguo72@outlook.com>
2026-02-28 19:58:51 +0800

    Site updated: 2026-02-28 19:58:50

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1765628070273";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1772279909196";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
